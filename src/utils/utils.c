baefd55f1273a6cc7cf842ff08a9052b7bc60e78
EduardoCotta <dudufrancamg@gmail.com>
2021-03-23 21:27:51 -0300

    feat: add distanceBetweenPoints method in utils

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -1,5 +1,14 @@
-//
-// Created by Dell on 11/03/2021.
-//
-
+#include <stdlib.h>
 #include "utils.h"
+
+float generateRandomFloat(float min, float max){
+    float number = rand() / (float) RAND_MAX;
+    return min + number * ( max - min );
+}
+
+int generateRandomInt(int min, int max){
+    int number = rand();
+    return min + (number % ( max+1 - min ));
+}
+
+
--- src/utils/utils.h
+++ src/utils/utils.h
@@ -1,8 +1,7 @@
-//
-// Created by Dell on 11/03/2021.
-//
-
 #ifndef PDS_TP1_COMBATE_UTILS_H
 #define PDS_TP1_COMBATE_UTILS_H
+
+float generateRandomFloat(float min, float max);
+int generateRandomInt(int min, int max);
 
 #endif //PDS_TP1_COMBATE_UTILS_H
