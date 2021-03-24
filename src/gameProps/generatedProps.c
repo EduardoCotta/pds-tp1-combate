b81444dab19e7e247f5794cad45c0dfae9de762e
EduardoCotta <dudufrancamg@gmail.com>
2021-03-23 21:26:15 -0300

    add generated static props methods

--- src/gameProps/generatedProps.c
+++ src/gameProps/generatedProps.c
@@ -1,4 +1,56 @@
-//
-// Created by Dell on 11/03/2021.
-//
+#include "generatedProps.h"
+#include "../draw/colors.h"
+
+
+RectangularObstacle getPreDefinedRectangular(Point superiorLeft, Point inferiorRight, struct ALLEGRO_COLOR color){
+    RectangularObstacle rectangle = {
+            .A = {
+                    .x = superiorLeft.x,
+                    .y = superiorLeft.y,
+            },
+            .B = {
+                .x = inferiorRight.x,
+                .y = superiorLeft.y,
+            },
+            .C = {
+                    .x = inferiorRight.x,
+                    .y = inferiorRight.y,
+            },
+            .D = {
+                    .x = superiorLeft.x,
+                    .y = inferiorRight.y,
+            },
+            .color = color
+    };
+
+    return rectangle;
+}
+
+GeneratedObstacles getPreDefinedObstacles(GameVariables variables){
+    GeneratedObstacles obstacles;
+    Colors colors = getColors();
+
+    Point A = {
+            .x = 2 * (variables.SCREEN_HORIZONTAL_FRACTION),
+            .y = 0.9 * (variables.SCREEN_VERTICAL_FRACTION)
+    };
+
+    Point B = {
+            .x = 3 * (variables.SCREEN_HORIZONTAL_FRACTION),
+            .y = 3 * (variables.SCREEN_VERTICAL_FRACTION)
+    };
+
+    obstacles.rectangularObstacles[0] = getPreDefinedRectangular(A, B, colors.ORANGE);
+
+    B.x =  3.5 * (variables.SCREEN_HORIZONTAL_FRACTION);
+
+    obstacles.rectangularObstacles[1] = getPreDefinedRectangular(A, B, colors.YELLOW);
+
+    B.x =  4 * (variables.SCREEN_HORIZONTAL_FRACTION);
+    B.y =  3.10 * (variables.SCREEN_VERTICAL_FRACTION);
+
+    obstacles.rectangularObstacles[2] = getPreDefinedRectangular(A, B, colors.PINK);
+
+    return obstacles;
+}
 
--- src/gameProps/generatedProps.h
+++ src/gameProps/generatedProps.h
@@ -1,8 +1,9 @@
-//
-// Created by Dell on 11/03/2021.
-//
-
 #ifndef PDS_TP1_COMBATE_GENERATEDPROPS_H
 #define PDS_TP1_COMBATE_GENERATEDPROPS_H
+
+#include "gameProps.h"
+#include "../generalConfigs/gameVariables.h"
+
+GeneratedObstacles getPreDefinedObstacles(GameVariables variables);
 
 #endif //PDS_TP1_COMBATE_GENERATEDPROPS_H
