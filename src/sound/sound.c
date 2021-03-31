4a44cc9f5c796ebbc138ac54ab6da9458e6143b1
EduardoCotta <dudufrancamg@gmail.com>
2021-03-31 18:06:36 -0300

    feat: add sound handle methods

--- src/sound/sound.c
+++ src/sound/sound.c
@@ -3,3 +3,30 @@
 //
 
 #include "sound.h"
+
+void loadSamples(GameSoundSamples* gameSoundSamples){
+    ALLEGRO_PATH *path = al_get_standard_path(ALLEGRO_RESOURCES_PATH);
+    al_append_path_component(path, "assets");
+    al_change_directory(al_path_cstr(path, '/'));
+
+    al_reserve_samples(4);
+
+    gameSoundSamples->shot = al_load_sample("sound/missile.wav");
+    gameSoundSamples->explosion = al_load_sample("sound/explosion.wav");
+    gameSoundSamples->soundtrack = al_load_sample("sound/game_song.ogg");
+    gameSoundSamples->soundtrack_instance = al_create_sample_instance(gameSoundSamples->soundtrack);
+
+    al_set_sample_instance_playmode(gameSoundSamples->soundtrack_instance, ALLEGRO_PLAYMODE_LOOP);
+    al_attach_sample_instance_to_mixer(gameSoundSamples->soundtrack_instance, al_get_default_mixer());
+
+}
+void playSample(ALLEGRO_SAMPLE* sample){
+    al_play_sample(sample, 1, 0, 1, ALLEGRO_PLAYMODE_ONCE, 0);
+}
+void playSampleInstance(ALLEGRO_SAMPLE_INSTANCE* sampleInstance){
+    al_play_sample_instance(sampleInstance);
+}
+
+void stopSampleInstance(ALLEGRO_SAMPLE_INSTANCE* sampleInstance){
+    al_stop_sample_instance(sampleInstance);
+}
--- src/sound/sound.h
+++ src/sound/sound.h
@@ -5,4 +5,11 @@
 #ifndef PDS_TP1_COMBATE_SOUND_H
 #define PDS_TP1_COMBATE_SOUND_H
 
+#include "../generalConfigs/gameVariables.h"
+
+void loadSamples(GameSoundSamples* gameSoundSamples);
+void playSample(ALLEGRO_SAMPLE* sample);
+void playSampleInstance(ALLEGRO_SAMPLE_INSTANCE* sampleInstance);
+void stopSampleInstance(ALLEGRO_SAMPLE_INSTANCE* sampleInstance);
+
 #endif //PDS_TP1_COMBATE_SOUND_H
