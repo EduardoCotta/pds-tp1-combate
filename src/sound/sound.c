fa4ffa67ccdc2d65c45e87fb212b188a557b9a80
EduardoCotta <dudufrancamg@gmail.com>
2021-03-31 18:04:55 -0300

    feat: add song assets

---
+++ src/sound/sound.c
@@ -0,0 +1,5 @@
+//
+// Created by Dell on 30/03/2021.
+//
+
+#include "sound.h"
---
+++ src/sound/sound.h
@@ -0,0 +1,8 @@
+//
+// Created by Dell on 30/03/2021.
+//
+
+#ifndef PDS_TP1_COMBATE_SOUND_H
+#define PDS_TP1_COMBATE_SOUND_H
+
+#endif //PDS_TP1_COMBATE_SOUND_H
